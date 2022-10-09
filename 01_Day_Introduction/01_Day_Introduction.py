6f6b696f52647d3952934a23e3ba16d2a84e58cf
Duong Trung Kien <96287101+codingwithkien@users.noreply.github.com>
2022-10-09 20:38:17 +0700

    Update 01_Day_Introduction.py

--- 01_Day_Introduction/01_Day_Introduction.py
+++ 01_Day_Introduction/01_Day_Introduction.py
@@ -1,21 +1,21 @@
 # Introduction
 # Day 1 - 30DaysOfPython Challenge
 
-print(2 + 3)   # addition(+)
-print(3 - 1)   # subtraction(-)
+print(5 + 3)   # addition(+)
+print(4 - 1)   # subtraction(-)
 print(2 * 3)   # multiplication(*)
-print(3 / 2)   # division(/)
-print(3 ** 2)  # exponential(**)
+print(6 / 2)   # division(/)
+print(2 ** 2)  # exponential(**)
 print(3 % 2)   # modulus(%)
-print(3 // 2)  # Floor division operator(//)
+print(6 // 2)  # Floor division operator(//)
 
 # Checking data types
 
 print(type(10))                  # Int
 print(type(3.14))                # Float
 print(type(1 + 3j))              # Complex
-print(type('Asabeneh'))          # String
+print(type('Kien'))          # String
 print(type([1, 2, 3]))           # List
-print(type({'name':'Asabeneh'})) # Dictionary
+print(type({'name':'Kien'}))     # Dictionary
 print(type({9.8, 3.14, 2.7}))    # Set
 print(type((9.8, 3.14, 2.7)))    # Tuple
