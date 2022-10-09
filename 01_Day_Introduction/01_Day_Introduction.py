36dd01eb25af6e31ffafb793c485e87bfce87eb2
Duong Trung Kien <96287101+codingwithkien@users.noreply.github.com>
2022-10-09 20:38:42 +0700

    Update 01_Day_Introduction.py

--- 01_Day_Introduction/01_Day_Introduction.py
+++ 01_Day_Introduction/01_Day_Introduction.py
@@ -13,7 +13,7 @@ print(6 // 2)  # Floor division operator(//)
 print(type(10))                  # Int
 print(type(3.14))                # Float
 print(type(1 + 3j))              # Complex
-print(type('Kien'))          # String
+print(type('Kien'))              # String
 print(type([1, 2, 3]))           # List
 print(type({'name':'Kien'}))     # Dictionary
 print(type({9.8, 3.14, 2.7}))    # Set
